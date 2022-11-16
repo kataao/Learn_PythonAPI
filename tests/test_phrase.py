6846a4bebb362c2b13955c0636b01720eff74e5e
kataao <n.che@list.ru>
2022-11-16 20:26:19 +0300

    exercises 10, 14 - corrections - add code to empty files

--- tests/test_phrase.py
+++ tests/test_phrase.py
@@ -1,4 +1,4 @@
-class TestShortPhrase:
-    def test_phrase(self):
+class TestPhrase:
+    def test_short_phrase(self):
         phrase = input("Set a phrase: ")
         assert len(phrase) < 15, "The phrase length should be less than 15 simbols"
